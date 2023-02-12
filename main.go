fd2a8383d46c9e09b215ad095e5e9a40277f82df
Prayash Koirala <prayashkoirala07@gmail.com>
2023-02-12 15:54:20 +0545

    data type in go

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	userName := "Prayash"
+	var userTickets int
+
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your ticket here to attend")
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 
 }
