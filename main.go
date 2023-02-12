9253ce7e663df5295b73a4523bd4f1af4e3b5733
Prayash Koirala <prayashkoirala07@gmail.com>
2023-02-12 20:12:35 +0545

    array example

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
-	userName := "Prayash"
-	var userTickets int
+	var conferenceTickets uint = 50
+	var remainingTickets uint = 50
+	var userName string
+	var userTickets uint
+	var bookings [50]string
+	bookings[0] = "John"
+	bookings[1] = "Alice"
+	fmt.Println(bookings)
 
 	fmt.Printf("Welcome to %v booking application \n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
-	userTickets = 2
+	fmt.Println("Enter you name")
+	fmt.Scan(&userName)
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
 
 	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Number of tickets remaining %v", remainingTickets)
 
 }
